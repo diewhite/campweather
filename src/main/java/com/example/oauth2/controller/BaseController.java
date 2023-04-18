96226083d1f9721b5100269ae460287fdfe00ce0
Kim Gyumin <diewhite@icloud.com>
2023-04-18 16:37:46 +0900

    fixed bugs wrong connect to login screen

--- src/main/java/com/example/oauth2/controller/BaseController.java
+++ src/main/java/com/example/oauth2/controller/BaseController.java
@@ -52,21 +52,25 @@ public class BaseController {
 
     @SuppressWarnings("unchecked")
     @GetMapping("/login")
-    public String getLoginPage(Model model) throws Exception {
-        Iterable<ClientRegistration> clientRegistrations = null;
-        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
-                .as(Iterable.class);
-        if (type != ResolvableType.NONE &&
-                ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
-            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
-        }
-        assert clientRegistrations != null;
-        clientRegistrations.forEach(registration ->
-                oauth2AuthenticationUrls.put(registration.getClientName(),
-                        authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
-        model.addAttribute("urls", oauth2AuthenticationUrls);
- 
-        return "login";
+    public String getLoginPage(Model model, HttpSession session) throws Exception {
+    	Member user = (Member) session.getAttribute("user");
+    	if(user==null) {
+    		Iterable<ClientRegistration> clientRegistrations = null;
+            ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+                    .as(Iterable.class);
+            if (type != ResolvableType.NONE &&
+                    ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+                clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+            }
+            assert clientRegistrations != null;
+            clientRegistrations.forEach(registration ->
+                    oauth2AuthenticationUrls.put(registration.getClientName(),
+                            authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+            model.addAttribute("urls", oauth2AuthenticationUrls);
+            return "login";
+    	} else {
+    		return "redirect:/";
+    	}
     }
     
     @GetMapping(value = "/login/{oauth2}")
